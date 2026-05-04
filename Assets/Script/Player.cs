ec66b6d8b235ae3b99f22682fef235e0ba96ab66
SophiaNaltchadjian <147109359+SophiaNaltchadjian@users.noreply.github.com>
2026-05-04 17:33:23 -0300

    Dash

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,23 +1,63 @@
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+    private Rigidbody2D rb;
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.1f;
+    public float dashCooldown = 0.1f;
+    bool isDashing;
+    bool canDash = true;
+    TrailRenderer trailRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        trailRenderer = GetComponent<TrailRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+        Dash();
         Move();
     }
     private void Move()
     {
         Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         transform.position += movement * speed * Time.deltaTime;
+    }
+    private void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && canDash)
+        {
+            StartCoroutine(DashCoroutine());
+        }
+    }
+    IEnumerator DashCoroutine()
+    {
+        canDash = false;
+        isDashing = true;
+
+        trailRenderer.emitting = true;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x, y);
+        rb.linearVelocity = direction.normalized * dashSpeed; //dash movement
+        yield return new WaitForSeconds(dashDuration);
+        rb.linearVelocity = new Vector2(0f,0f);
+        isDashing = false;
+        trailRenderer.emitting = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash= true;  
     }
 }
